b216e857835bec3c1e8ca5cbeadead8087ecbb9a
soburi <soburi@gmail.com>
2016-04-23 12:28:10 +0900

    Remove request handling. Print and Indicate WiFi connect status.

--- Arduino/FakePicoWS/FakePicoWS.ino
+++ Arduino/FakePicoWS/FakePicoWS.ino
@@ -26,6 +26,7 @@
 #define PASS "password"
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * json, size_t length);
+void sendSensorValue(int A0_current, int IO0_current);
 
 bool strequal(const char* x, const char* y) {
   return (strncmp(x, y, strlen(x)) == 0);
@@ -55,6 +56,8 @@ void setup() {
   pinMode(0, INPUT_PULLUP);
   pinMode(14, OUTPUT);
 
+  digitalWrite(14, 0);
+  
   A0_prev = analogRead(A0);
   IO0_prev = digitalRead(0);
   A0_current = A0_prev;
@@ -62,18 +65,15 @@ void setup() {
   
   Serial.println();
 
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.print("[SETUP] BOOT WAIT ");
-    Serial.println(t);
-    Serial.flush();
-    delay(1000);
-  }
-
   WiFiMulti.addAP(SSID, PASS);
 
   while (WiFiMulti.run() != WL_CONNECTED) {
     delay(100);
   }
+
+  Serial.print("WiFi connected: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(14, 1);
 
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
@@ -89,8 +89,8 @@ void loop() {
   if ( (last_update + SEND_INTERVAL) < now) {
     last_update = now;
         
-    int A0_current = analogRead(A0);
-    int IO0_current = digitalRead(0);
+    A0_current = analogRead(A0);
+    IO0_current = digitalRead(0);
 
     bool A0_changed = false;
     bool IO0_changed = false;
@@ -106,28 +106,7 @@ void loop() {
     }
     
     if(A0_changed || IO0_changed) {
-
-      
-      StaticJsonBuffer<128> notifyBuffer;
-      JsonObject& notify = notifyBuffer.createObject();
-      JsonObject& notifyBody = notify.createNestedObject("notify");
-
-      notifyBody["slider"] = (int) (A0_current / 10.24);
-      notifyBody["button"] = (IO0_current ? 0 : 1);
-      
-      notify.printTo(sendtext, sizeof(sendtext) );
-
-      for(int i=0; i<WEBSOCKETS_SERVER_CLIENT_MAX; i++) {
-        if( conn_status[i] == WStype_CONNECTED) {
-          Serial.print("sendTXT[");
-          Serial.print(i);
-          Serial.print("]: ");
-          Serial.println(sendtext);
-          webSocket.sendTXT(i, sendtext);
-        }
-      }
-
-      notify.remove("notify");
+      sendSensorValue(A0_current, IO0_current);
     }
   }
 }
@@ -136,9 +115,6 @@ void loop() {
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * json, size_t length) {
   IPAddress ip = webSocket.remoteIP(num);
-
-  StaticJsonBuffer<128> responseBuffer;
-  JsonObject& response = responseBuffer.createObject();
 
   Serial.print("webSocketEvent ");
   switch (type) {
@@ -157,6 +133,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * json, size_t length) {
       Serial.print(ip);
       Serial.print(" url: ");
       Serial.println((char*)json);
+      sendSensorValue(A0_current, IO0_current);
       return;
     }
     case WStype_TEXT: {
@@ -164,44 +141,6 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * json, size_t length) {
       Serial.print(num);
       Serial.print("] get Text: ");
       Serial.println((char*)json);
-
-      StaticJsonBuffer<128> receiveBuffer;
-      JsonObject& root = receiveBuffer.parseObject( (char*)json);
-
-      response["response"] = "xxx";
-      response["error"] = -1;
-      response["value"] = -1;
-
-      do { 
-        if (!root.success()) {
-          Serial.println("parseObject() failed");
-          response["error"] = -1;
-          break;
-        }
-
-        const char* reqparam = root["request"];
-        if (reqparam == NULL)
-        {
-          Serial.println("invalid request");
-          response["error"] = -2;
-          break;
-        }
-
-        Serial.print("request: ");
-        Serial.println(reqparam);
-        response["response"] = reqparam;
-
-        if ( strequal("led", reqparam) ) {
-          int ledstat = root["param"];
-          digitalWrite(14, (ledstat == 0) ? 0 : 1);
-          response["value"] = (ledstat == 0) ? 0 : 1;
-        }
-        else {
-          response["value"] = -1;
-          response["error"] = -3;
-        }
-        response["error"] = 0;
-      } while(false);
       break;
     }
     case WStype_BIN: {
@@ -210,19 +149,34 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * json, size_t length) {
       Serial.print("] get binary length: ");
       Serial.println(length);
       hexdump(json, length);
-      response["error"] = -4;
       break;
     }
     case WStype_ERROR: {
       Serial.println("ERROR");
-      response["error"] = -5;
       break;
     }
   }
+}
 
-  response.printTo(sendtext, sizeof(sendtext) );
-  Serial.print("sendTXT: ");
-  Serial.println(sendtext);
-  webSocket.sendTXT(num, sendtext);
+
+void sendSensorValue(int slider, int button) {
+  StaticJsonBuffer<128> notifyBuffer;
+  JsonObject& root = notifyBuffer.createObject();
+  JsonObject& notify = root.createNestedObject("notify");
+
+  notify["slider"] = (int) (slider / 1024.0 * 100.0);
+  notify["button"] = (button ? 0 : 1);
+  
+  root.printTo(sendtext, sizeof(sendtext) );
+
+  for(int i=0; i<WEBSOCKETS_SERVER_CLIENT_MAX; i++) {
+    if( conn_status[i] == WStype_CONNECTED) {
+      Serial.print("sendTXT[");
+      Serial.print(i);
+      Serial.print("]: ");
+      Serial.println(sendtext);
+      webSocket.sendTXT(i, sendtext);
+    }
+  }
 }
 
